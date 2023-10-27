c1dcc875f8da5ee451c38b9ddb3f4b393cc3a31d
geromuhaya <132812722+geromuhaya@users.noreply.github.com>
2023-10-27 08:30:12 +0100

    Delete assets directory

--- assets/javascript/script.js
+++
@@ -1,8 +0,0 @@
-
-
-$(document).ready(function(){
-    $('.menu-toggle').click(function(){
-        $('.menu-toggle').toggleClass('active');
-        $('.nav-bar').toggleClass('active');
-    })
-})
